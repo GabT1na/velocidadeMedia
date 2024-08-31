26100b4ff770a6c5e65754d90de42268ccf584c1
Gabrielly Cristina <167761882+GabT1na@users.noreply.github.com>
2024-08-31 22:37:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa feito para calcular a velocidade média km/h");
+    System.out.println();
+    System.out.println("A distância percorrida foi de:");
+    double distancia = 400;
+    System.out.println(distancia);
+    System.out.println();
+    System.out.println("O tempo gasto foi de:");
+    double tempo = 3;
+    System.out.println(tempo);
+System.out.println();
+    System.out.println("A velocidade média é:");
+    double velocidadeMedia = distancia / tempo;
+    System.out.println(velocidadeMedia);
+    
+  }
+}
